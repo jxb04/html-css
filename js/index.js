0a5f5e53aea9d3d174327744257360be9d276b97
Joel Bernasky <joel_bernasky@hotmail.com>
2018-09-24 09:17:56 -0400

    variables and types

--- js/index.js
+++ js/index.js
@@ -1,8 +1,25 @@
 /*
 index.js
 */
+
+"use strict";
+
 var msg = "hello javascript";
 console.log(msg);
 
 var resultsDiv = document.getElementById("results");
 resultsDiv.innerHTML = "<p>This is from JavaScript.</p>";
+
+console.log("msg is " + typeof(msg));
+console.log("resultsDiv is " + typeof(resultsDiv));
+
+var none;
+console.log("none is " + typeof(none));
+
+var aNumber = 0;
+console.log("aNumber is " + typeof(aNumber));
+
+var trueFalse = true;
+console.log("trueFalse is " + typeof(trueFalse));
+
+// nonexistent = "this shouldn't work";
