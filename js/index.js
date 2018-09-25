fb851b66a3025db31563419eb5c55644e69ec359
jxb04 <joel_bernasky@hotmail.com>
2018-09-24 21:22:01 -0400

    objects and arrays

--- js/index.js
+++ js/index.js
@@ -10,71 +10,121 @@ console.log(msg);
 var resultsDiv = document.getElementById("results");
 resultsDiv.innerHTML = "<p>This is from JavaScript.</p>";
 
-console.log("msg is " + typeof(msg));
-console.log("resultsDiv is " + typeof(resultsDiv));
+// var result = {
+//     name: "jQuery",
+//     language: "JavaScript",
+//     score: 4.5,
+//     showLog: function() {
 
-var none;
-console.log("none is " + typeof(none));
+//     },
+//     owner: {
+//         login: "joelbernasky",
+//         id: 123456
+//     }
+// };
 
-var aNumber = 10;
-console.log("aNumber is " + typeof(aNumber));
+// result.phoneNumber = "123-456-7890";
 
-var trueFalse = true;
-console.log("trueFalse is " + typeof(trueFalse));
+// console.log(result.phoneNumber);
 
-// nonexistent = "this shouldn't work";
+var results = [{
+    name: "jQuery",
+    language: "JavaScript",
+    score: 4.5,
+    showLog: function() {
 
-if (!none) {
-    console.log("none is undefined");
-}
+    },
+    owner: {
+        login: "joelbernasky",
+        id: 123456
+    }
+}, {
+    name: "jQuery UI",
+    language: "JavaScript",
+    score: 3.5,
+    showLog: function() {
 
-if (aNumber === "10") {
-    console.log ("10 is 10");
-}
-
-// function showMsg(msg) {
-//     console.log("showMsg: " + msg);
-// }
-
-function showMsg(msg, more) {
-    if (more) {
-        console.log("showMsg+:" + msg + more);
-    } else {
-        console.log("showMsg+:" + msg);
+    },
+    owner: {
+        login: "joelbernasky",
+        id: 123456
     }
 }
+];
 
-showMsg("some information");
-showMsg("more information", " and even more");
+console.log(results.length);
+console.log(results[0].name);
 
-var showIt = function (msg) {
-    console.log(msg);
-}
+// results.push(result);
+// results.push({
+//     name: "dummy project"
+// })
 
-showIt("some message");
+// console.log("msg is " + typeof(msg));
+// console.log("resultsDiv is " + typeof(resultsDiv));
 
-function showItThen(msg, callback) {
-    showIt(msg);
-    callback();
-}
+// var none;
+// console.log("none is " + typeof(none));
 
-showItThen("showItThen called", function () {
-    console.log("callback called");
-});
+// var aNumber = 10;
+// console.log("aNumber is " + typeof(aNumber));
 
-var inGlobal = true;
+// var trueFalse = true;
+// console.log("trueFalse is " + typeof(trueFalse));
 
-function testMe() {
-    console.log("testMe(): " + inGlobal);
+// // nonexistent = "this shouldn't work";
 
-    var someMsg = "some message";
-    console.log("testMe(): " + someMsg);
+// if (!none) {
+//     console.log("none is undefined");
+// }
 
-    showItThen("with Closure", function () {
-        showIt("testMe With Closure(): "+ someMsg);
-    });
-}
+// if (aNumber === "10") {
+//     console.log ("10 is 10");
+// }
 
-//console.log("global: " + someMsg);
+// // function showMsg(msg) {
+// //     console.log("showMsg: " + msg);
+// // }
 
-testMe();
+// function showMsg(msg, more) {
+//     if (more) {
+//         console.log("showMsg+:" + msg + more);
+//     } else {
+//         console.log("showMsg+:" + msg);
+//     }
+// }
+
+// showMsg("some information");
+// showMsg("more information", " and even more");
+
+// var showIt = function (msg) {
+//     console.log(msg);
+// }
+
+// showIt("some message");
+
+// function showItThen(msg, callback) {
+//     showIt(msg);
+//     callback();
+// }
+
+// showItThen("showItThen called", function () {
+//     console.log("callback called");
+// });
+
+// var inGlobal = true;
+
+// function testMe() {
+//     console.log("testMe(): " + inGlobal);
+
+//     var someMsg = "some message";
+//     console.log("testMe(): " + someMsg);
+
+//     showItThen("with Closure", function () {
+//         showIt("testMe With Closure(): "+ someMsg);
+//     });
+// }
+
+// //console.log("global: " + someMsg);
+
+// testMe();
